4b2953ece6ef9a82510ddf62370dac978e3a3861
Von Harris <vonharris00@gmail.com>
2023-11-18 03:17:48 -0500

    Exercise 3

---
+++ world.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    let btn = document.querySelector('#lookup');  
+
+    btn.addEventListener('click', ()=>{
+        console.log("Lookup button clicked");     
+
+        let entry = document.querySelector('#country').value;
+
+         const params = new URLSearchParams({country: entry});
+
+         const url = `http://localhost/info2180-lab5/world.php?${params.toString()}`;
+
+         let stateObj = {id:"100"};
+         window.history.pushState(stateObj, "Country Query", url);
+
+
+         fetch(url, {method: 'GET'})
+             .then(response => {
+                 console.log(response);
+                     if (response.ok) {
+                         return response.text()
+                     } else {
+                         return Promise.reject("Something went wrong")
+                     }
+             })
+             .then(data => {
+                     let results = document.querySelector('#result');
+                     results.innerHTML = data;       
+             })
+             .catch(error => console.log(error));
+
+})
+})
+
+
--- world.php
+++ world.php
@@ -1,15 +1,19 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
+$country = $_GET['country'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
 
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+
+
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
