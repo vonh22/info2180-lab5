b3f75431a0a3d2b590b0757ec191dd5cca746b32
Von Harris <vonharris00@gmail.com>
2023-11-18 23:26:39 -0500

    Exercise 4

--- world.php
+++ world.php
@@ -4,18 +4,25 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 $country = $_GET['country'];
-
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
-
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+if (!empty($results)){
+  echo "<table border = '1'>";
+    echo "<tr>";
+        echo "<th>Country Name</th>";
+        echo "<th>Continent</th>";
+        echo "<th>Independence Year</th>";
+        echo "<th>Head of State</th>";
+    echo "</tr>";
+      foreach ($results as $row){
+        echo "<tr><td>" . $row['name'] . "</td><td>" . $row['continent'] . "</td><td>" . $row['independence_year'] . "</td><td>" . $row['head_of_state'] . "</td></tr>";
+      }
+      echo "</table>";
+    }
+    else{
+      echo "No Match";
+    }
+
 ?>
-
-
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
